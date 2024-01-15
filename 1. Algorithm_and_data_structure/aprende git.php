498bd96c6001882e8955f94e976b7085e6dac745
Fidel Pereira <marfilhofap@gmail.com>
2024-01-16 08:28:27 +0900

    comit ida tan

---
+++ 1. Algorithm_and_data_structure/aprende git.php
@@ -0,0 +1,16 @@
+<?php
+
+/** 
+ * git status
+ * git add . / git add naran.php : muda ba staging area 
+ * git commit -m "komentariu ba commit"
+ * git diff
+ * git log / git log --oneline
+ * git restore . / git restore naran.php
+ * git restore -s
+ * git reset --hard id_commit
+ * git checkout -b naran_branches
+ * 
+ * 
+ * 
+ */
